aa88e434f324879f3ef0232c54853b0f383f5d94
fiety2121 <132485649+fiety2121@users.noreply.github.com>
2024-02-02 11:40:24 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Table } from './table';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { Home } from './Home';
-// import { Update } from './update'; // Adjust the filename based on your actual file
-// import { Create } from './create'; // Adjust the filename based on your actual file
-// import { Read } from './read'; // Adjust the filename based on your actual file
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Table/>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path='/tp2/Home' element={<Home />} />
-          <Route path='/tp2/update' element={<Update />} />
-          <Route path='/tp2/create' element={<Create />} />
-          <Route path='/tp2/read' element={<Read />} />
-        </Routes>
-      </BrowserRouter> */}
     </div>
   );
 }
